406b850a2dba28ea9161020989b7f1d8c568bd54
Marina <masset.marina@live.fr>
2022-12-28 12:26:47 +0100

    added color picker

--- JS/main.js
+++ JS/main.js
@@ -9,29 +9,35 @@ function TurnOnDarkMode() {
 
 TurnOnDarkMode();
 
-function colorPicker() {
-  var p = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-  for(var i = 0; i < p.length; i++)
-  {
-      for(var j = 0; j < p.length; j++)
-      {
-          for(var k = 0; k < p.length; k++)
-          {
-              var x = i - k * 0.5;
-              var y = j - k * 0.25;
-              background[x][y] = "#" + p[i] + p[j] + p[k];
-          }
+const pickr = Pickr.create({
+  el: '.color-picker',
+  theme: 'classic',
+  default: '#ffffff',
+
+  swatches: [
+      '#333333',
+      '#BF1212'
+  ],
+
+  components: {
+
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+          hex: true,
+          rgba: true,
+          input: true,
+          clear: true,
+          save: true
       }
   }
+});
 
-  var div = document.querySelector(".colorPicker");
-  for (var l = 0; l < maxX; l++)
-  {
-      for (var m = 0; m < maxY; m++)
-      {
-          div.innerHTML ='<div style="background:#' + background[x][y] +'"></div>';
-      }
-
-      //https://www.youtube.com/watch?v=FX1xb1cim7E 
-  }
-}
+pickr.on('change', (color) => {
+  const rgbaColor = color.toRGBA().toString()
+  document.querySelector('body').style.background = rgbaColor;
+});
